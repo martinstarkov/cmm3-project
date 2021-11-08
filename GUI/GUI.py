fa6ae7b3d3233f2b2f207a9ac6d88712a7587d1b
fzizzle <63846425+fzizzle@users.noreply.github.com>
2021-11-08 14:17:30 +0000

    Implemented Output to JSON for new GUI. It is now functional.

--- GUI/GUI.py
+++ GUI/GUI.py
@@ -5,7 +5,7 @@ import json
 import task_a
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 import matplotlib.pyplot as plt
-from gui_inputs import *
+import input_boxes
 
 global root
 root = tk.Tk()
@@ -24,8 +24,8 @@ class InputBox():
         self.column_span = column_span
     
     def switch(self):
+        # handles toggle button functionality
         global switch_is_on
-        
         if switch_is_on:
             self.toggle.config(image = off)
             switch_is_on = False
@@ -34,6 +34,7 @@ class InputBox():
             switch_is_on = True
 
     def place(self):
+        # places the button in the GUI window
         input_label = tk.Label(root, text = self.label)
         input_label.grid(column = self.column, row = self.row)
         if self.toggle == True:
@@ -44,8 +45,14 @@ class InputBox():
             self.input_box.grid(column = self.column + 1, row = self.row)
 
     def get_input(self):
-        data = self.input_box.get()
-        return data
+        #Takes the input from the entry box and returns it.
+        if self.toggle and switch_is_on:
+            return 'True'
+        elif self.toggle and not switch_is_on:
+            return 'False'
+        else:
+            data = self.input_box.get()
+            return data
 
 class Task():
     """Manages buttons and functions managing the buttons"""
@@ -56,7 +63,7 @@ class Task():
         self.column_span = column_span
 
     def go_to_task(self):
-        #Sets up new page for the task specific inputs and elements
+        #Clears elements on the page and replaces them with task appropriate elements.
         for widgets in root.winfo_children():
             widgets.destroy()
         canvas = tk.Canvas(root, height=500, width=800)
@@ -66,29 +73,36 @@ class Task():
         header.grid(columnspan=10, column=0, row=1)
         instructions = tk.Label(text="Define your variables before running the script.")
         instructions.grid(columnspan=10, column=0, row=2)
-        self.inputs = place_input_boxes(self.label)
+        self.inputs, self.input_dict = place_input_boxes(self.label)
         btn_run = tk.Button(root, text="Run Simulation", padx=10, pady=10, fg="black", bg="pink", command=self.run)
         btn_run.grid(columnspan=10, column=0, row=9, sticky = tk.W+tk.E)
         btn_back = tk.Button(root, text="Back to Homepage", padx=10, pady=10, fg="black", bg="pink", command=back)
         btn_back.grid(columnspan=10, column=0, row=10, sticky = tk.W+tk.E)
     
     def run(self):
-        #Reads inputs then runs script
+        #Handles activity of the Run Button, stores user inputs in a data.json before running script.
+        dict_copy = self.input_dict.copy()
+        for input in self.inputs:
+            value = input.get_input()
+            label = input.label
+            dict_copy[label] = value
+        dict_copy['id'] = self.label
+        with open(os.path.join(os.path.dirname(__file__), 'data.json'), 'w') as convert_file:
+            convert_file.write(json.dumps(dict_copy))
         for widgets in root.winfo_children():
             widgets.destroy()
-        for input in self.inputs:
-            input.get_input()
-            #TODO write to file function
+            #TODO write inputs to file
         task_a.animated_particle_diffusion(root, back, task_a.steps, task_a.h, task_a.x_min, task_a.x_max, task_a.y_min, task_a.y_max, task_a.fluid_coordinates, task_a.spatial_field, task_a.field_vectors, task_a.fluid_concentrations, task_a.color_dictionary)
     
     def place_button(self):
-        #Places the button on the homepage
+        #Places the task button on the main landing page
         btn = tk.Button(root, text=str(self.label), padx=10, pady=10, fg="black", bg="pink", command=self.go_to_task)
         btn.grid(columnspan=10, column=0, row=self.row, sticky = tk.W+tk.E)
 
 
 def init_buttons(list):
     # Takes a list and element type (Input Box or Button) and generates the element using the respective class
+    tasks = ["A", "B", "C", "D", "E"]
     row_num = 2
     for task in tasks:
         element = Task(task, row_num)
@@ -98,57 +112,27 @@ def init_buttons(list):
 
 
 def place_input_boxes(task_label):
-    # Reads file and gets all the inputs, then checks for toggle or input box. Places input boxes/toggles
+    # Reads input boxes file, then checks for toggle or input box. Places input boxes/toggles.
     row_num = 3
-    list = []
-    for key in Task_Dict[task_label]:
-        if Task_Dict[task_label][key][0] == "toggle":
+    boxes = []
+    input_dict = input_boxes.Task_Dict[task_label]
+    for key in input_dict:
+        if input_boxes.Task_Dict[task_label][key][0] == "toggle":
             element = InputBox(4, row_num, key, toggle=True)
         else:
             element = InputBox(4, row_num, key)
-        list.append(element)
+        boxes.append(element)
         element.place()
         row_num += 1
-
-
-def init_text_boxes(list):
-    # Takes a list and element type (Input Box or Button) and generates the element using the respective class
-    row_num = 3
-    for header in input_headers:
-        if (input_headers.index(header) == 0):
-            column_num = 0
-            row_num = 3
-        elif ((input_headers.index(header)+1) % 2 == 0):
-            column_num = 2
-            row_num = row_num
-        else:
-            column_num = 0
-            row_num += 1
-        element = InputBox(column_num, row_num, header)
-        list.append(element)
-        element.place()
-    
-
-def clear_previous_inputs():
-    open(os.path.join(os.path.dirname(__file__), 'data.json'), 'w').close()
+    return boxes, input_dict
 
 def back():
-    pass
-    # TODO: update this
+    # Handles the activity of the back button
     for widgets in root.winfo_children():
       widgets.destroy()
     main(root)
 
-
-def task(task_letter):
-    #TODO Edit this to allow for multiple tasks
-    for widgets in root.winfo_children():
-      widgets.destroy()
-    init_text_boxes(input_headers, InputBox)
-    print("Running task " + str(task_letter))
-    #task_a.animated_particle_diffusion(root, back, task_a.steps, task_a.h, task_a.x_min, task_a.x_max, task_a.y_min, task_a.y_max, task_a.fluid_coordinates, task_a.spatial_field, task_a.field_vectors, task_a.fluid_concentrations, task_a.color_dictionary)
-
-
+ 
 def place_logo():
     # places logo and instructions for the gui
     logo = Image.open(os.path.join(os.path.dirname(__file__), 'logo.png'))
@@ -156,10 +140,6 @@ def place_logo():
     logo_label = tk.Label(image = logo, master=root)
     logo_label.image = logo
     logo_label.grid(columnspan=10, column=0, row=0)
-
-
-input_headers = ["Max Time (s)", "Time Step/ dT (s)", "Number of Particles (Integer)", "Diffusivity (Float)", "Velocity Field (True/False)", "Simulation Type (1D/ 2D/ Live)" ]
-tasks = ["A", "B", "C", "D", "E"]
 
 
 def main(root):
