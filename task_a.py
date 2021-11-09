d1648ef65ccd3b14408bc055520fcf207a1f0ace
Martin <emailregisterlogin@gmail.com>
2021-11-09 17:05:35 +0000

    Make color map a passed variable

--- task_a.py
+++ task_a.py
@@ -13,18 +13,17 @@ X = 0
 Y = 1
 
 class Simulation(object):
-    def __init__(self, dt, t_max, color_dictionary, \
-                 x_min, x_max, y_min, y_max, particle_count, \
-                 cell_width, cell_height, diffusivity, \
-                 velocity_coordinates=None, velocity_vectors=None):
+    def __init__(self, dt, t_max, x_min, x_max, y_min, y_max, 
+                 particle_count, cell_width, cell_height, diffusivity, 
+                 color_map, velocity_coordinates=None, velocity_vectors=None):
         self.dt = dt
         self.steps = int(t_max / self.dt) + 1
-        self.color_dictionary = color_dictionary
         self.min = np.array([x_min, y_min])
         self.max = np.array([x_max, y_max])
         self.particle_count = particle_count
         self.cell_size = np.array([cell_width, cell_height])
         self.diffusivity = diffusivity
+        self.cmap = color_map
         self.velocity_coordinates = velocity_coordinates
         self.velocity_vectors = velocity_vectors
         if self.velocity_coordinates is not None:
@@ -40,12 +39,11 @@ class Simulation(object):
         self.figure, self.axes = plt.subplots()
         self.axes.set_xlim(self.min[X], self.max[X])
         self.axes.set_ylim(self.min[Y], self.max[Y])
-        cmap = matplotlib.colors.ListedColormap(self.color_dictionary.values())
-        self.figure.colorbar(matplotlib.cm.ScalarMappable(cmap=cmap))
+        self.figure.colorbar(matplotlib.cm.ScalarMappable(cmap=self.cmap))
         self.scatter = self.axes.scatter(self.coordinates[:, X], self.coordinates[:, Y])
+        self.scatter.set_cmap(self.cmap)
         self.axes.set_title("Time: " + str(0))
         self.scatter.set_array(self.particles)
-        self.scatter.set_cmap(cmap)
         
     def __diffuse(self, velocities):
         # TODO: Check  that this formula matches the one in the slides.
@@ -113,9 +111,8 @@ class Simulation(object):
         concentration = self.__get_concentrations()
         self.heatmap = self.axes.imshow(concentration, animated=True, extent=(self.min[X], self.max[X], self.min[Y], self.max[Y]))
         
-        cmap = matplotlib.colors.LinearSegmentedColormap.from_list("custom_cmap", list(self.color_dictionary.values()), 256)
-        self.heatmap.set_cmap(cmap)
-        self.figure.colorbar(matplotlib.cm.ScalarMappable(cmap=cmap))
+        self.heatmap.set_cmap(self.cmap)
+        self.figure.colorbar(matplotlib.cm.ScalarMappable(cmap=self.cmap))
         
         anim = animation.FuncAnimation(self.figure, self.__concentration_animation, frames=self.steps, interval=1, repeat=False)    
         plt.show()   
@@ -131,10 +128,9 @@ class Simulation(object):
         self.heatmap = self.axes.imshow(concentration, extent=(self.min[X], self.max[X], self.min[Y], self.max[Y]))
         
         self.axes.set_title("Time: " + str(round((self.steps - 1) * self.dt, 3)))
-        
-        cmap = matplotlib.colors.LinearSegmentedColormap.from_list("custom", list(color_dictionary.values()), N=256)
-        self.heatmap.set_cmap(cmap)
-        self.figure.colorbar(matplotlib.cm.ScalarMappable(cmap=cmap))
+
+        self.heatmap.set_cmap(self.cmap)
+        self.figure.colorbar(matplotlib.cm.ScalarMappable(cmap=self.cmap))
         
         plt.show()
 
@@ -143,9 +139,6 @@ class Simulation(object):
             plt.quiver(self.velocity_coordinates[:, X], self.velocity_coordinates[:, Y], \
                        self.velocity_vectors[:, X], self.velocity_vectors[:, Y])
             plt.show()
-
-    def get_color_value(self, color):
-        return list(self.color_dictionary.keys()).index(color)
 
     def add_rectangle(self, bottom_left, size, value: int):
         bound_x = np.logical_and(self.coordinates[:, X] >= bottom_left[X], self.coordinates[:, X] <= bottom_left[X] + size[X])
@@ -164,19 +157,21 @@ def read_data_file(file, *args):
 # 0 is red, 1 is blue
 color_dictionary = {
     "red": (1, 0, 0),
-    "blue": (0, 0, 1)
+    "blue": (0, 0, 1),
 }
 
 # Read vector field data from file.
 velocity_coordinates, velocity_vectors = read_data_file("velocityCMM3.dat", [0, 1], [2, 3])
 
+#cmap = matplotlib.colors.ListedColormap(color_dictionary.values())
+cmap = matplotlib.colors.LinearSegmentedColormap.from_list("gradient", list(color_dictionary.values()), 256)
 
-sim = Simulation(0.01, 0.2, color_dictionary, -1, 1, -1, 1, 65536, 64, 64, 0.01, velocity_coordinates, velocity_vectors)
+sim = Simulation(0.01, 0.2, -1, 1, -1, 1, 65536, 64, 64, 0.01, cmap, velocity_coordinates, velocity_vectors)
 
 #sim.display_vector_field()
 
-#sim.add_circle([0.0, 0.0], 0.25, sim.get_color_value("blue"))
+#sim.add_circle([0.0, 0.0], 0.25, 1)
 
-sim.add_rectangle([-1, -1], [1, 2], sim.get_color_value("blue"))
+sim.add_rectangle([-1, -1], [1, 2], 1)
 
 sim.animated_concentration()
