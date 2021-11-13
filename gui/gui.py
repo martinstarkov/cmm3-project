5cce602f576698828116c521748a5fc9fde1c064
fzizzle <63846425+fzizzle@users.noreply.github.com>
2021-11-13 22:32:58 +0000

    Autopep8'd it

--- gui/gui.py
+++ gui/gui.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-from tkinter.filedialog import askopenfile 
+from tkinter.filedialog import askopenfile
 import os
 from PIL import Image, ImageTk
 import json
@@ -9,59 +9,69 @@ global root
 root = tk.Tk()
 
 # Global variables for toggle switch
-on = ImageTk.PhotoImage(file = os.path.join(os.path.dirname(__file__), "on.png"))
-off = ImageTk.PhotoImage(file = os.path.join(os.path.dirname(__file__), "off.png"))
+on = ImageTk.PhotoImage(file=os.path.join(os.path.dirname(__file__), "on.png"))
+off = ImageTk.PhotoImage(file=os.path.join(
+    os.path.dirname(__file__), "off.png"))
 switch_is_on = True
+
 
 class InputBox():
     """This takes every variable type and creates a label and a text box for it. //
     It also has methods to capture and store the data before running the simulation"""
-    def __init__(self, column, row, label, data_type, toggle = False, column_span = 1):
+
+    def __init__(self, column, row, label, data_type, toggle=False, column_span=1):
         self.column = column
         self.row = row
         self.label = label
         self.data_type = data_type
         self.toggle = toggle
         self.column_span = column_span
-    
+
     def switch(self):
         # handles toggle button functionality
         global switch_is_on
         if switch_is_on:
-            self.toggle.config(image = off)
+            self.toggle.config(image=off)
             switch_is_on = False
             if self.label == 'Velocity Field':
                 self.browse.destroy()
         else:
-            self.toggle.config(image = on)
+            self.toggle.config(image=on)
             switch_is_on = True
             if self.label == 'Velocity Field':
-                self.browse = tk.Button(root, text ='Open File', command = lambda:self.open_file()) 
-                self.browse.grid(columnspan=1, column=self.column + 2, row = self.row)
-    
-    def open_file(self): 
-        self.file = askopenfile(mode ='r', filetypes =[('velocity field', '*.dat')]).name
+                self.browse = tk.Button(
+                    root, text='Open File', command=lambda: self.open_file())
+                self.browse.grid(
+                    columnspan=1, column=self.column + 2, row=self.row)
+
+    def open_file(self):
+        self.file = askopenfile(mode='r', filetypes=[
+                                ('velocity field', '*.dat')]).name
         if self.file is not None:
             self.file_path = str(pathlib.PurePath(str(self.file)))
             success_text = tk.Label(text="File Loaded!")
-            success_text.grid(columnspan=10, column=self.column + 3, row= self.row)
-            
+            success_text.grid(
+                columnspan=10, column=self.column + 3, row=self.row)
+
     def place(self):
         # places the button in the GUI window
-        input_label = tk.Label(root, text = self.label)
-        input_label.grid(column = self.column, row = self.row)
+        input_label = tk.Label(root, text=self.label)
+        input_label.grid(column=self.column, row=self.row)
         if self.toggle:
-            self.toggle = tk.Button(root, image = on, bd = 0, command = self.switch)
-            self.toggle.grid(columnspan=1, column=self.column + 1, row = self.row)
+            self.toggle = tk.Button(root, image=on, bd=0, command=self.switch)
+            self.toggle.grid(
+                columnspan=1, column=self.column + 1, row=self.row)
             if self.label == 'Velocity Field':
-                self.browse = tk.Button(root, text ='Open File', command = lambda:self.open_file()) 
-                self.browse.grid(columnspan=1, column=self.column + 2, row = self.row)
+                self.browse = tk.Button(
+                    root, text='Open File', command=lambda: self.open_file())
+                self.browse.grid(
+                    columnspan=1, column=self.column + 2, row=self.row)
         else:
             self.input_box = tk.Entry(root)
-            self.input_box.grid(column = self.column + 1, row = self.row)
+            self.input_box.grid(column=self.column + 1, row=self.row)
 
     def get_input(self):
-        #Takes the input from the entry box and returns it.
+        # Takes the input from the entry box and returns it.
         if self.toggle and switch_is_on:
             return True
         elif self.toggle and not switch_is_on:
@@ -73,16 +83,18 @@ class InputBox():
             data = self.input_box.get()
             return float(data)
 
+
 class Task():
     """Manages each tasks landing page and inputs."""
-    def __init__(self, task, row, column_span = 10):
+
+    def __init__(self, task, row, column_span=10):
         self.task = task
         self.row = row
         self.label = "Task " + (self.task)
         self.column_span = column_span
 
     def go_to_task(self):
-        #Clears elements on the page and replaces them with task appropriate elements.
+        # Clears elements on the page and replaces them with task appropriate elements.
         for widgets in root.winfo_children():
             widgets.destroy()
         canvas = tk.Canvas(root, height=500, width=800)
@@ -90,17 +102,19 @@ class Task():
         place_logo()
         header = tk.Label(text=self.label)
         header.grid(columnspan=10, column=0, row=1)
-        instructions = tk.Label(text="Define your variables before running the script.")
+        instructions = tk.Label(
+            text="Define your variables before running the script.")
         instructions.grid(columnspan=10, column=0, row=2)
         self.inputs, self.input_dict = place_input_boxes(self.label)
-        btn_run = tk.Button(root, text="Run Simulation", padx=10, pady=10, fg="black", bg="pink", command=self.run)
-        btn_run.grid(columnspan=10, column=0, row=9, sticky = tk.W+tk.E)
-        btn_back = tk.Button(root, text="Back to Homepage", padx=10, pady=10, fg="black", bg="pink", command=back)
-        btn_back.grid(columnspan=10, column=0, row=10, sticky = tk.W+tk.E)
-    
-    
+        btn_run = tk.Button(root, text="Run Simulation", padx=10,
+                            pady=10, fg="black", bg="pink", command=self.run)
+        btn_run.grid(columnspan=10, column=0, row=9, sticky=tk.W+tk.E)
+        btn_back = tk.Button(root, text="Back to Homepage",
+                             padx=10, pady=10, fg="black", bg="pink", command=back)
+        btn_back.grid(columnspan=10, column=0, row=10, sticky=tk.W+tk.E)
+
     def run(self):
-        #Handles activity of the Run Button, stores user inputs in a data.json before running script.
+        # Handles activity of the Run Button, stores user inputs in a data.json before running script.
         dict = self.input_dict
         for input in self.inputs:
             value = input.get_input()
@@ -120,12 +134,13 @@ class Task():
             json.dump(dictionary, json_file, indent=4)
         for widgets in root.winfo_children():
             widgets.destroy()
-        #TODO replace this with each tasks function.
-    
+        # TODO replace this with each tasks function.
+
     def place_button(self):
-        #Places the task button on the main landing page
-        btn = tk.Button(root, text=str(self.label), padx=10, pady=10, fg="black", bg="pink", command=self.go_to_task)
-        btn.grid(columnspan=10, column=0, row=self.row, sticky = tk.W+tk.E)
+        # Places the task button on the main landing page
+        btn = tk.Button(root, text=str(self.label), padx=10,
+                        pady=10, fg="black", bg="pink", command=self.go_to_task)
+        btn.grid(columnspan=10, column=0, row=self.row, sticky=tk.W+tk.E)
 
 
 def init_buttons(list):
@@ -159,27 +174,28 @@ def place_input_boxes(task_label):
     return boxes, task_dict
 
 
-
 def back():
     # Handles the activity of the back button
     for widgets in root.winfo_children():
-      widgets.destroy()
+        widgets.destroy()
     main(root)
 
-def open_file(): 
-    file = askopenfile(mode ='r', filetypes =[('velocity field', '*.dat')]) 
+
+def open_file():
+    file = askopenfile(mode='r', filetypes=[('velocity field', '*.dat')])
     if file is not None:
         success_text = tk.Label(text="File Loaded!")
         success_text.grid(columnspan=10, column=7, row=5)
-        content = file.read() 
-        with open('velocityfield.dat','w') as data: 
+        content = file.read()
+        with open('velocityfield.dat', 'w') as data:
             data.write(content)
- 
+
+
 def place_logo():
     # places logo and instructions for the gui
     logo = Image.open(os.path.join(os.path.dirname(__file__), 'logo.png'))
-    logo = ImageTk.PhotoImage(logo, master = root)
-    logo_label = tk.Label(image = logo, master=root)
+    logo = ImageTk.PhotoImage(logo, master=root)
+    logo_label = tk.Label(image=logo, master=root)
     logo_label.image = logo
     logo_label.grid(columnspan=10, column=0, row=0)
 
@@ -194,6 +210,7 @@ def main(root):
     buttons = []
     init_buttons(buttons)
     root.mainloop()
+
 
 if __name__ == "__main__":
     main(root)
