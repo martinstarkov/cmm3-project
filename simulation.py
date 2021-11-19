64c149b820e350937cd00fe01c62935c370a8699
Martin <emailregisterlogin@gmail.com>
2021-11-19 03:43:10 +0000

    Add assertions to check for initial conditions, SWITCHED DT AND T_MAX, fixed boundary conditions, removed unnecessary plotting, added read_data_file check

--- simulation.py
+++ simulation.py
@@ -1,10 +1,9 @@
-from scipy import spatial
 from scipy.spatial import cKDTree
-from matplotlib import animation
 import matplotlib.colors
 import numpy as np
 import matplotlib.pyplot as plt
 import math
+import os
 
 # TODO: Make colors options for the user (in case of color blindness) by passing the dictionary accessors as function parameters.
 # TODO: BUG: Concentration vs particle fields spin in opposite directions.
@@ -13,11 +12,23 @@ X = 0
 Y = 1
 
 class Simulation(object):
-    def __init__(self, dt, t_max, x_min, x_max, y_min, y_max, 
+    def __init__(self, t_max, dt, x_min, x_max, y_min, y_max, 
                  particle_count, cell_width, cell_height, diffusivity, 
                  color_map=None, velocity_coordinates=None, velocity_vectors=None):
-        self.dt = dt
+        assert t_max > 0, "Max time must be greater than 0"
+        assert dt > 0, "Time step must be greater than 0"
         assert dt <= t_max, "Time step cannot exceed maximum time"
+        assert x_max > x_min, "X_max must be greater than X_min"
+        assert y_max > y_min, "Y_max must be greater than Y_min"
+        assert particle_count > 0, "Particle count must be greater than 0"
+        assert cell_width > 0, "Cell width must be greater than 0"
+        assert cell_height > 0, "Cell height must be greater than 0"
+        assert diffusivity >= 0, "Diffusivity must be greater than or equal to 0"
+        self.dt = dt
+        # Let us consider an example situation: dt = 0.6, t_max = 1.0, 
+        # in this case the code will stop after two steps: 0.0 and 0.6.
+        # If the user wishes to continue to 1.2 (exceeding t_max by 1)
+        # they can round the fraction below before converting to an int.
         self.steps = int(t_max / self.dt) + 1
         self.min = np.array([x_min, y_min])
         self.max = np.array([x_max, y_max])
@@ -44,6 +55,9 @@ class Simulation(object):
     def __boundary_conditions(self):
         self.coordinates[:] = np.where(self.coordinates < self.min, 2 * self.min - self.coordinates, self.coordinates)
         self.coordinates[:] = np.where(self.coordinates > self.max, 2 * self.max - self.coordinates, self.coordinates)
+        # If bouncing coordinate still takes it out of bounds, set it to the bound.
+        self.coordinates[:] = np.where(self.coordinates < self.min, self.min, self.coordinates)
+        self.coordinates[:] = np.where(self.coordinates > self.max, self.max, self.coordinates)
         
     def __update(self):
         if self.velocity_coordinates is not None and self.velocity_vectors is not None:
@@ -54,15 +68,20 @@ class Simulation(object):
             self.__lagrangian(0)
         self.__boundary_conditions()
 
-    def __concentration_animation(self, step: int):
-        self.__update()
-        self.axes.set_title("Time: " + str(round(step * self.dt, 3)))
+    def init_animation(self):
         self.calculate_concentrations()
         self.heatmap.set_array(self.concentrations)
 
-    def simulate(self):
+    def animate(self, step: int):
+        self.axes.set_title("Time: " + str(round(step * self.dt, 3)))
+        self.calculate_concentrations()
+        self.heatmap.set_array(self.concentrations)
+        self.__update()
+
+    def simulate(self, print_time=False):
         for step in range(self.steps):
-            print("Simulation time: " + str(round(step * self.dt, 3)))
+            if print_time:
+                print("Simulation time: " + str(round(step * self.dt, 3)))
             self.__update()
             
     def calculate_concentrations(self):
@@ -87,17 +106,14 @@ class Simulation(object):
             self.heatmap.set_cmap(self.cmap)
             self.figure.colorbar(matplotlib.cm.ScalarMappable(cmap=self.cmap))
 
-    def animated_concentration(self):
+    def setup_animated_plot(self):
         self.__setup_concentration_plot(True)
         self.axes.set_title("Time: " + str(0))
-        anim = animation.FuncAnimation(self.figure, self.__concentration_animation, frames=self.steps, interval=1, repeat=False)    
-        plt.show()   
 
-    def static_concentration(self):
-        self.simulate()
+    def setup_static_plot(self):
+        self.simulate(print_time=True)
         self.__setup_concentration_plot(False)
         self.axes.set_title("Time: " + str(round((self.steps - 1) * self.dt, 3)))
-        plt.show()
 
     def display_vector_field(self):
         if self.velocity_coordinates is not None and self.velocity_vectors is not None:
@@ -114,6 +130,32 @@ class Simulation(object):
         distances = (self.coordinates[:, X] - center[X]) ** 2 + (self.coordinates[:, Y] - center[Y]) ** 2
         self.particles = np.where(distances < radius ** 2, value, self.particles)
 
+def read_data_file(file, *columns):
+    data = []
+    for column in columns:
+        try:
+            data.append(np.genfromtxt(file, usecols=tuple(column), invalid_raise=True))
+        except:
+            print("Could not retrieve " + str(column) + " columns from file " + str(os.path.basename(file)))
+            return [None for column in columns]
+    return data
 
-def read_data_file(file, *args):
-    return [np.loadtxt(file, usecols=tuple(c)) for c in args]
+def setup(time, step, diffusivity, particle_count, domain, cell_size, animated, velocity_info, circle_info, rectangle_info, colors):
+
+    # Read vector field data from file.
+    if velocity_info[0]:
+        velocity_coordinates, velocity_vectors = read_data_file(velocity_info[1], [0, 1], [2, 3])
+    else:
+        velocity_coordinates = velocity_vectors = None
+
+    cmap = matplotlib.colors.LinearSegmentedColormap("", colors, 256)
+
+    sim = Simulation(time, step, domain[0], domain[1], domain[2], domain[3], particle_count, cell_size[0], cell_size[1], diffusivity, cmap, velocity_coordinates, velocity_vectors)
+
+    if circle_info[0]:
+        sim.add_circle([circle_info[2], circle_info[3]], circle_info[4], circle_info[1])
+    
+    if rectangle_info[0]:
+        sim.add_rectangle([rectangle_info[2], rectangle_info[4]], [rectangle_info[3], rectangle_info[5]], rectangle_info[1])
+        
+    return sim
