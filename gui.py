004b73a8a39f40e93c6742dd3923a0f567e801ca
Martin <emailregisterlogin@gmail.com>
2021-11-19 03:41:12 +0000

    Added engineering spill task animation and embedded plots

--- gui.py
+++ gui.py
@@ -1,12 +1,20 @@
 import tkinter as tk
+from tkinter import messagebox
 from tkinter.filedialog import askopenfile
-import os
-import numpy as np
 from PIL import Image, ImageTk
+from matplotlib import animation
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from sys import platform as sys_pf
+import matplotlib.pyplot as plt
+import numpy as np
+import os
 import json
-import pathlib
+import simulation
 
-import task_a
+# Tkinter embedded plot fix for macOS.
+if sys_pf == 'darwin':
+    import matplotlib
+    matplotlib.use("TkAgg")
 
 # Utility functions for easily creating gridded tkinter widgets.
 
@@ -74,6 +82,7 @@ class UserInterface(object):
             self.create_header()
             self.create_menu_buttons()
             self.root.mainloop()
+
         
     def create_header(self):
         # Create the GUI logo.
@@ -89,9 +98,9 @@ class UserInterface(object):
         set_column_sizes(self.container, [100], uniform="container")
         set_row_sizes(self.container, [33, 33, 33], uniform="container")
         # Create individual buttons in the container.
-        CustomSpecifications("Custom Specifications", self, 0)
+        CustomConditions("Custom Conditions", self, 0)
         ValidationTasks("Validation Tasks", self, 1)
-        EngineeringSpill("Real-time Engineering Spill", self, 2)
+        EngineeringSpill("Engineering Spill", self, 2)
 
     # Creates the container cell.
     def create_container(self):
@@ -103,6 +112,17 @@ class UserInterface(object):
             for widget in self.container.winfo_children():
                 widget.destroy()
         self.label["text"] = self.label_text
+        
+    def create_plot(self, figure, reset_function, back_function):
+        canvas = FigureCanvasTkAgg(figure, master=self.container)
+        canvas.get_tk_widget().grid(row=0, column=0, padx=0, pady=0)
+        
+        create_button(self.container, "Reset Plot", 1, 0, reset_function, pady=(5, 0), ipady=15, fg="black", bg="pink")
+        create_button(self.container, "Back", 2, 0, back_function, pady=(5, 0), ipady=15, fg="black", bg="pink")
+        
+        set_row_sizes(self.container, [80, 4, 4])
+        set_column_sizes(self.container, [100])
+        return canvas
       
 # Abstract class which represents an input row.
 class InputField(object):
@@ -320,7 +340,7 @@ class MainMenuButton(object):
         self.ui.clear_container()
         self.ui.label["text"] = self.name
           
-class CustomSpecifications(MainMenuButton):
+class CustomConditions(MainMenuButton):
     def press(self):
         super().press()
         self.inputs = []
@@ -336,7 +356,7 @@ class CustomSpecifications(MainMenuButton):
             self.create_input_object(key, field_info, row)
         
         create_button(self.ui.container, "Run Simulation", row_count - 1, 0, self.run, pady=(5, 0), ipady=10, fg="black", bg="pink")
-        create_button(self.ui.container, "Back to Homepage", row_count, 0, self.ui.create_menu_buttons, pady=(5, 0), ipady=10, fg="black", bg="pink")
+        create_button(self.ui.container, "Back", row_count, 0, self.ui.create_menu_buttons, pady=(5, 0), ipady=10, fg="black", bg="pink")
         
     def create_input_object(self, text, field_info, row):
         type = field_info["type"]
@@ -352,29 +372,96 @@ class CustomSpecifications(MainMenuButton):
         self.inputs.append(input)
         
     def run(self):
-        o = []
+        self.outputs = []
+        type_validation = True
         for input in self.inputs:
-            outputs = []
+            entry_outputs = []
             types = []
             for key in input.field_info["default"]:
                 types.append(type(key))
             for index, entry in enumerate(input.entries):
                 if isinstance(entry, bool) or isinstance(entry, str):
-                    outputs.append(types[index](entry))
+                    entry_outputs.append(types[index](entry))
                 elif entry.winfo_class() == 'Entry':
-                    outputs.append(types[index](entry.get()))
-            o.append(outputs)
-        task_a.custom_specifications(o[0][0], o[1][0], o[2][0], o[3][0], o[4], o[5], o[6], o[7], o[8], o[9])
+                    try:
+                        entry_outputs.append(types[index](entry.get()))
+                    except Exception as e:
+                        type_validation = False
+                        messagebox.showerror('error', e)
+                        
+            self.outputs.append(entry_outputs)
+        if type_validation and self.validation():
+            self.setup()
+        
+    def setup(self):
+        self.ui.clear_container()
+        time_max = self.outputs[0][0]
+        self.ui.label["text"] = self.name
+        animated = self.outputs[6][0]
+        # Color gradient which goes red to blue (0.0 to 1.0).
+        color_gradient = dict(red   = [(0.0, 0.0, 1.0), (1.0, 0.0, 0.0)],
+                              green = [(0.0, 0.0, 0.0), (1.0, 0.0, 0.0)],
+                              blue  = [(0.0, 0.0, 0.0), (1.0, 1.0, 0.0)])
+        # time, step, diffusivity, particle_count, domain, cell_size, animated, velocity_info, circle_info, rectangle_info, color_gradient
+        sim = simulation.setup(time_max, self.outputs[1][0], self.outputs[2][0], self.outputs[3][0], self.outputs[4], \
+                               self.outputs[5], animated, self.outputs[7], self.outputs[8], self.outputs[9], color_gradient)
+        if animated:
+            sim.setup_animated_plot()
+            anim = animation.FuncAnimation(sim.figure, func=sim.animate, init_func=sim.init_animation, frames=sim.steps, interval=1, repeat=False)
+            self.ui.label["text"] = self.name + " (animating until t = " +  str(time_max) + "s)"
+        else:
+            sim.setup_static_plot()
+            
+        self.ui.create_plot(sim.figure, self.setup, self.press).draw()
+    
+    # Returns true if condition is met, otherwise prompts user with an error message and returns false.
+    def check(self, condition, message):
+        return True if condition else not bool(messagebox.showinfo("Field error", message))
+        
+    # Validates entry field inputs and notifies the user if any or all of them do not fit requiremed criteria.
+    # Criteria such as certain values being positive, velocity data being readable, etc. 
+    def validation(self):
+        # All conditions must be met in order for validity to hold.
+        valid = self.check(self.outputs[0][0] > 0, "Max time must be greater than 0") & \
+                self.check(self.outputs[1][0] > 0, "Time step must be greater than 0") & \
+                self.check(self.outputs[1][0] < self.outputs[0][0], "Time step cannot exceed maximum time") & \
+                self.check(self.outputs[2][0] > 0, "Diffusivity must be greater than or equal to 0") & \
+                self.check(self.outputs[3][0] > 0, "Particle count must be greater than 0") & \
+                self.check(self.outputs[4][1] > self.outputs[4][0], "X_max must be greater than X_min") & \
+                self.check(self.outputs[4][3] > self.outputs[4][2], "Y_max must be greater than Y_min") & \
+                self.check(self.outputs[5][0] > 0, "Cell width must be greater than 0") & \
+                self.check(self.outputs[5][1] > 0, "Cell height must be greater than 0")
+        if self.outputs[7][0]: # Velocity field is being used.
+            # velocity path can be read and has 4 columns
+            vc, vf = simulation.read_data_file(self.outputs[7][1], [0, 1], [2, 3])
+            valid &= self.check(vc is not None and vf is not None, "Velocity field data file does not contain the required data columns") 
+        if self.outputs[8][0]: # Circle field is being used.
+            valid &= self.check(self.outputs[8][1] == 0 or self.outputs[8][1] == 1, "Circle concentration must start as 0 or 1 (red or blue)")
+        if self.outputs[9][0]: # Rectangle field is being used.
+            valid &= self.check(self.outputs[9][1] == 0 or self.outputs[9][1] == 1, "Rectangle concentration must start as 0 or 1 (red or blue)")
+        return valid
     
 # TODO: Make this work.
 class ValidationTasks(MainMenuButton):
     def press(self):
         super().press()
 
-# TODO: Make this work.
 class EngineeringSpill(MainMenuButton):
     def press(self):
         super().press()
-
-
+        time_max = 5
+        self.ui.label["text"] = self.name + " (animating until t = " +  str(time_max) + "s)"
+        # Color gradient which goes blue to green to green (0.0 to 0.3 to 1.0).
+        color_gradient = dict(blue   = [(0.0, 0.0, 1.0), (0.3, 0.0, 1.0), (0.3, 0.0, 0.0),  (1.0, 0.0, 0.0)],
+                              green = [(0.0, 0.0, 0.0), (0.3, 0.7, 1.0), (1.0, 1.0, 1.0)],
+                              red  = [(0.0, 0.0, 0.0), (0.3, 0.0, 1.0), (0.3, 0.0, 0.0),  (1.0, 0.0, 0.0)])
+        # time, step, diffusivity, particle_count, domain, cell_size, animated, velocity_info, circle_info, rectangle_info, color_gradient
+        sim = simulation.setup(time_max, 0.01, 0.1, 15000, [-1, 1, -1, 1], [75, 75], [True], \
+                               [True, os.path.join(os.path.dirname(__file__), "velocityCMM3.dat")], \
+                               [True, 1, 0.4, 0.4, 0.1], [False], color_gradient)
+        sim.setup_animated_plot()
+        anim = animation.FuncAnimation(sim.figure, func=sim.animate, init_func=sim.init_animation, frames=sim.steps, interval=1, repeat=False)
+            
+        self.ui.create_plot(sim.figure, self.press, self.ui.create_menu_buttons).draw()
+        
 UserInterface(os.path.join(os.path.dirname(__file__), "gui/input_boxes.json"))
